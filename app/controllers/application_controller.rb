17ce4303bebbf3d6534dd845833b5e84ff0b1d8e
Mikolaj Grygiel <mikolaj0grygiel@gmail.com>
2020-07-07 21:19:45 +0200

    Revert "Test"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # We n't want to overcomplicate, so let this be just disabled
+  # We don't want to overcomplicate, so let this be just disabled
   # protect_from_forgery with: :exception
 end
